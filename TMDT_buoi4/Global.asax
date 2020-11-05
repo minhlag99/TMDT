d2250de29421963b037f103c88d25bd44db62909
minhlag99 <66405955+minhlag99@users.noreply.github.com>
2020-11-06 03:25:22 +0700

    first commit

---
+++ TMDT_buoi4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TMDT_buoi4.MvcApplication" Language="C#" %>
---
+++ TMDT_buoi4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TMDT_buoi4.Startup))]
+namespace TMDT_buoi4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
